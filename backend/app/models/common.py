8ccdd31902edd1a79825c25e4b77a46ca93b371a
Baptiste Schelle <bschelle69@gmail.com>
2026-06-19 09:53:28 +0200

    Refacto common and subsequent compound

--- backend/app/models/common.py
+++ backend/app/models/common.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import Optional, Literal
+from typing import Optional, List, Literal
 from enum import Enum
 from datetime import datetime
+
+# 1 - Shared enumerated types
 
 class RoleEnum(str, Enum):
     """The role of a species in the reaction."""
@@ -22,7 +24,42 @@ class UnitEnum(str, Enum):
     MOLES = "moles"
     MILLIMOLES = "millimoles"
 
+class PhaseEnum(str, Enum):
+    """Physical phase"""
+    GAS = "gas"
+    LIQUID = "liquid"
+    SOLID = "solid"
+    SOLUTION = "solution"
+
+class SectionEnum(str, Enum):
+    """Article/protocol section"""
+    EXPERIMENTAL = "experimental"
+    RESULTS = "results"
+    METHODS = "methods"
+    SUPPORTING = "supporting"
+    OTHER = "other"
+
+# 2 - Shared base types
+
 class QtyBase(BaseModel):
     "Base for a quantity w/ units"
     value: float = Field(..., gt=0)
     unit: UnitEnum = Field(...)
+
+class CompoundRefBase(BaseModel):
+    """Base model for a chemical compound (reference)."""
+    compound_id: int
+    preferred_name: str = Field(..., description="Canonical name of compound")
+    commercial_name: Optional[str] = Field(None, description="Commercial or usual name")
+    formula: Optional[str] = Field(None, description="Chemical formula")
+    smiles: Optional[str] = Field(None, description="SMILES canonical")
+    molecular_weight: Optional[float] = Field(None, gt=0, description="Molecular weight (in g/mol)")
+
+class SpeciesQtyBase(BaseModel):
+    qty : Optional[QtyBase] = None
+    moles : Optional[float] = Field(None, gt=0, description="Amount of substance (in moles)")
+    millimoles: Optional[float] = Field(None, gt=0, description="Amount of substance (in millimoles)")
+    equivalents: Optional[float] = Field(None, gt=0, description="Number of equivalents in respect to limiting reactant")
+    concentration: Optional[float] = Field(None, gt=0, description="Concentration of solutant (in mol/L²)")
+    density: Optional[float] = Field(None, gt=0, description="Density (in kg/m3)")
+    purity: Optional[float] = Field(None, ge=0, le=100, description="Purity (in molar %)")
--- backend/app/models/compound.py
+++ backend/app/models/compound.py
@@ -8,28 +8,28 @@ class CompoundBase(BaseModel):
     formula: Optional[str] = Field(None, description="Chemical formula")
     smiles: Optional[str] = Field(None, description="SMILES canonical")
     cas_number: Optional[str] = Field(None, description="CAS number")
-    molecular_weight: Optional[float] = Field(None, gt=0)
-    density: Optional[float] = Field(None, gt=0)
-    purity: Optional[float] = Field(None, ge=0, le=100)
-    synonyms: List[str] = Field(default_factory=List)
+    molecular_weight: Optional[float] = Field(None, gt=0, description="Molecular weight (in g/mol)")
+    density: Optional[float] = Field(None, gt=0, description="Density (in kg/m3)")
+    purity: Optional[float] = Field(None, ge=0, le=100, description="Purity (in molar %)")
+    synonyms: List[str] = Field(default_factory=list, description="List of known synonyms")
 
 class CompoundCreate(CompoundBase):
     pass
 
 class CompoundRead(CompoundBase):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = {"from_attributes": True}
 
     id: int
     created_at: datetime
     updated_at: Optional[datetime] = None
 
 class CompoundUpdate(CompoundBase):
-    preferred_name: Optional[str] =  Field(..., description="Canonical name of compound")
-    commercial_name: Optional[str] = None
-    formula: Optional[str] = None
-    smiles: Optional[str] = None
-    cas_number: Optional[str] = None
-    molecular_weight: Optional[float] = Field(None, gt=0)
-    density: Optional[float] = Field(None, gt=0)
-    purity: Optional[float] = Field(None, ge=0, le=100)
-    synonyms: Optional[List[str]] = Field(default_factory=List)
+    preferred_name: str = Field(..., description="Canonical name of compound")
+    commercial_name: Optional[str] = Field(None, description="Commercial or usual name")
+    formula: Optional[str] = Field(None, description="Chemical formula")
+    smiles: Optional[str] = Field(None, description="SMILES canonical")
+    cas_number: Optional[str] = Field(None, description="CAS number")
+    molecular_weight: Optional[float] = Field(None, gt=0, description="Molecular weight (in g/mol)")
+    density: Optional[float] = Field(None, gt=0, description="Density (in kg/m3)")
+    purity: Optional[float] = Field(None, ge=0, le=100, description="Purity (in molar %)")
+    synonyms: List[str] = Field(default_factory=list, description="List of known synonyms")
